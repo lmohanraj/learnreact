90092f40ffbd7f44a4f65b737b78c4695066f66c
lmohanraj <lmohanraj1997@gmail.com>
2022-03-02 18:26:52 +0530

    Basic fields for sign in & sign up

--- src/SignInUp/SignIn.js
+++ src/SignInUp/SignIn.js
@@ -1,5 +1,28 @@
+import "./SignIn.css";
+
 function SignIn() {
-  return <div>Sign In</div>;
+  return (
+    <section>
+      <form onSubmit={(evt) => evt.preventDefault()}>
+        <div className="inputs">
+          <label for="name">Username</label>
+          <input type="email" placeholder="Email" id="name" name="name" />
+        </div>
+        <div className="inputs">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <button className="submitSignIn" type="submit">
+          SignIn
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default SignIn;
--- src/SignInUp/SignUp.js
+++ src/SignInUp/SignUp.js
@@ -1,5 +1,46 @@
+import "./SignUp.css";
+
 function SignUp() {
-  return <div>Sign Up</div>;
+  return (
+    <section>
+      <form onSubmit={(evt) => evt.preventDefault()}>
+        <div className="inputs">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            id="name"
+            name="name"
+          />
+        </div>
+        <div className="inputs">
+          <label for="email">Email</label>
+          <input type="email" placeholder="Email" id="email" name="email" />
+        </div>
+        <div className="inputs">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="inputs">
+          <label for="cpassword">Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            id="cpassword"
+            name="cpassword"
+          />
+        </div>
+        <button className="submitSignIn" type="submit">
+          SignUp
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default SignUp;
